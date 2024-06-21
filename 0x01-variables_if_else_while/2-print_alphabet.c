91edb8f7fccd58e42d2f23c01e943b688e9f75cf
Rediet Geremew <redietgeremew18@gmail.com>
2024-06-21 10:26:25 -0700

    create print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - This is a description
+ */
+int main(void)
+{
+	char letter;
+	for (letter='a'; letter<='z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return 0;
+}
