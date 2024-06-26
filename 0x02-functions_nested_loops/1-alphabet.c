c38f42316628e9bd9d3207c06e7f72f2505186ad
Rediet Geremew <redietgeremew18@gmail.com>
2024-06-26 04:33:56 -0700

    update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,14 @@
 #include "main.h"
 
+/**
+ *print_alphabet - it prints a to z in lowercase*
+ *Return: void
+ */
+
 void print_alphabet(void)
 {
 	char *str = "abcdefghijklmnopqrstuvwxyz";
+	
 	while (*str)
 	{
 		_putchar(*str);
