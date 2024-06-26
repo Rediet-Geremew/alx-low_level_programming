ada619eb7ae7b67315c368e5d2135b4c88bcfd33
Rediet Geremew <redietgeremew18@gmail.com>
2024-06-26 07:26:38 -0700

    update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,7 @@
 
 /**
  * print_sign - a function that prints a sign of a number*
+ * @n: the number to be checked
  * Return: 1 if the number is greater than zero
  * 0 if the number is zero
  * -1 if the number is less than zero
