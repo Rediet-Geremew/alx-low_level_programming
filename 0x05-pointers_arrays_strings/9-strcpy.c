74c7796d9d5ee55e77f366b00e3baaf4523d9c6c
Rediet Geremew <redietgeremew18@gmail.com>
2024-07-01 10:56:52 -0700

    create 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - a function that copies the string pointed to by src
+ *
+ * @dest: buffer point
+ * @src: where the strings are copied
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *dest_start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (dest_start);
+}
