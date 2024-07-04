87aebb46d148e24188fc20a3e379338b7eacc613
Rediet Geremew <redietgeremew18@gmail.com>
2024-07-04 06:27:49 -0700

    create a strcat function

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: first string
+ * @src: second string
+ * Return: resulting string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *dest_ptr = dest;
+
+	while (*dest_ptr != '\0')
+	{
+		dest_ptr++;
+	}
+	while (*src != '\0')
+	{
+		*dest_ptr = *src;
+		dest_ptr++;
+		src++;
+	}
+	*dest_ptr = '\0';
+	return dest;
+}
