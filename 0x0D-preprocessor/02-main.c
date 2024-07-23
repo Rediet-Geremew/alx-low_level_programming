40d2e6e8492e65ebd7113c8d665dc1f5f1b50f25
Rediet Geremew <redietgeremew18@gmail.com>
2024-07-22 18:51:20 -0700

    use standard predefined macros

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#ifndef MAIN2_H
+#define MAIN2_H
+
+/**
+ * main - prints the name of the file
+ *
+ * Return: 0 on success
+ */
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
+#endif
