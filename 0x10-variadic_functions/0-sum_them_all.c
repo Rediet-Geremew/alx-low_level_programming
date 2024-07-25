cd89013ebe592baf57a466bb5921089c442b46fa
Rediet Geremew <redietgeremew18@gmail.com>
2024-07-25 03:27:18 -0700

    updated

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,16 +11,15 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int sum =0;
-	
+	va_list nums;
+	va_start(nums, n);
+
 	if (n == 0)
 	{
 		return (0);
 	}
 	else
 	{
-		va_list nums;
-		va_start(nums, n);
-
 		for (i = 0; i < n; i++)
 		{
 			int x = va_arg(nums, int);
